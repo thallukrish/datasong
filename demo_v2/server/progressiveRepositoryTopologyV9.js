1841fc8446bc8f38b8847d010a8bb52309d1175d
thallukrish <thallukrish@gmail.com>
2026-08-20 15:58:14 +0530

    Add deterministic entity schema extraction

--- demo_v2/server/progressiveRepositoryTopologyV9.js
+++ demo_v2/server/progressiveRepositoryTopologyV9.js
@@ -1,6 +1,7 @@
 import { ProgressiveRepositoryTopologyV7 } from './progressiveRepositoryTopologyV7.js';
 import { CallPathIndexerV3 } from './callPathIndexerV3.js';
 import { MoquiXmlExecutionAdapter } from './moquiXmlExecutionAdapter.js';
+import { MoquiEntitySchemaAdapter } from './moquiEntitySchemaAdapter.js';
 
 export class ProgressiveRepositoryTopologyV9 extends ProgressiveRepositoryTopologyV7 {
   constructor(options) {
@@ -9,14 +10,20 @@ export class ProgressiveRepositoryTopologyV9 extends ProgressiveRepositoryTopolo
     this.callPathIndex = null;
     this.moquiXmlAdapter = new MoquiXmlExecutionAdapter(this);
     this.moquiXmlExecution = null;
+    this.moquiEntitySchemaAdapter = new MoquiEntitySchemaAdapter(this);
+    this.moquiEntitySchema = null;
+    this.entitySchemas = [];
+    this.entitySchemaByName = new Map();
   }
 
   async prepare(repoUrl) {
     const prep = await super.prepare(repoUrl);
+    this.moquiEntitySchema = await this.moquiEntitySchemaAdapter.augment();
     this.moquiXmlExecution = await this.moquiXmlAdapter.augment();
     this.callPathIndex = this.callPathIndexer.build();
     return {
       ...prep,
+      moquiEntitySchema: this.moquiEntitySchema,
       moquiXmlExecution: this.moquiXmlExecution,
       callPathIndex: {
         version: this.callPathIndex.version,
@@ -27,6 +34,13 @@ export class ProgressiveRepositoryTopologyV9 extends ProgressiveRepositoryTopolo
         topPaths: this.callPathIndex.topPaths
       }
     };
+  }
+
+  entitySchema(name) {
+    if (!name) return null;
+    if (this.entitySchemaByName.has(name)) return this.entitySchemaByName.get(name);
+    const leaf = String(name).split(/[.#:/]/).at(-1);
+    return this.entitySchemaByName.get(leaf) || this.entitySchemas.find((s) => s.name === leaf || s.fullName === name) || null;
   }
 
   topCallPaths(limit = 10) {
