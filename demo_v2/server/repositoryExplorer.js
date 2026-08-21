c65dceb0d3541b4e8032d409666da541e58f808d
thallukrish <thallukrish@gmail.com>
2026-08-21 14:12:57 +0530

    refactor: drop superseded V45 scout layer

--- demo_v2/server/repositoryExplorer.js
+++ demo_v2/server/repositoryExplorer.js
@@ -1,4 +1,4 @@
-import { ProgressiveRepositoryExplorerV45 } from './progressiveRepositoryExplorerV45.js';
+import { ProgressiveRepositoryExplorerV44 } from './progressiveRepositoryExplorerV44.js';
 import { withMapPersistence } from './explorer/mapPersistence.js';
 import { withPersistedMap } from './explorer/persistedMap.js';
 import { withStructuredWorkflow } from './explorer/structuredWorkflow.js';
@@ -6,7 +6,7 @@ import { withSemanticModel } from './explorer/semanticModel.js';
 import { withBusinessPriorityScout } from './explorer/businessPriorityScout.js';
 import { withEntityReconciliation } from './explorer/entityReconciliation.js';
 
-const ExplorerWithMapPersistence = withMapPersistence(ProgressiveRepositoryExplorerV45);
+const ExplorerWithMapPersistence = withMapPersistence(ProgressiveRepositoryExplorerV44);
 const ExplorerWithPersistedMap = withPersistedMap(ExplorerWithMapPersistence);
 const ExplorerWithStructuredWorkflow = withStructuredWorkflow(ExplorerWithPersistedMap);
 const ExplorerWithSemanticModel = withSemanticModel(ExplorerWithStructuredWorkflow);
