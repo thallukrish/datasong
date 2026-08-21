ab1aac9e29b722ddfc68bfa7591436682ca0e10d
thallukrish <thallukrish@gmail.com>
2026-08-21 14:59:37 +0530

    refactor: drop obsolete V24 pre-admission traversal layer

--- demo_v2/server/repositoryExplorer.js
+++ demo_v2/server/repositoryExplorer.js
@@ -1,4 +1,4 @@
-import { ProgressiveRepositoryExplorerV24 } from './progressiveRepositoryExplorerV24.js';
+import { ProgressiveRepositoryExplorerV23 } from './progressiveRepositoryExplorerV23.js';
 import { withCallPathPreprocessLifecycle } from './explorer/callPathPreprocessLifecycle.js';
 import { withCallPathSeedPreprocessor } from './explorer/callPathSeedPreprocessor.js';
 import { withInitialCallPathClassifier } from './explorer/initialCallPathClassifier.js';
@@ -15,7 +15,7 @@ import { withSemanticModel } from './explorer/semanticModel.js';
 import { withBusinessPriorityScout } from './explorer/businessPriorityScout.js';
 import { withEntityReconciliation } from './explorer/entityReconciliation.js';
 
-const ExplorerWithPreprocessLifecycle = withCallPathPreprocessLifecycle(ProgressiveRepositoryExplorerV24);
+const ExplorerWithPreprocessLifecycle = withCallPathPreprocessLifecycle(ProgressiveRepositoryExplorerV23);
 const ExplorerWithSeedPreprocessor = withCallPathSeedPreprocessor(ExplorerWithPreprocessLifecycle);
 const ExplorerWithInitialClassifier = withInitialCallPathClassifier(ExplorerWithSeedPreprocessor);
 const ExplorerWithBusinessMap = withBusinessMapAccumulation(ExplorerWithInitialClassifier);
