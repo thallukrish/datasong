388aed8fa30756a64bb751c8b11358ced0ff20f0
thallukrish <thallukrish@gmail.com>
2026-08-21 13:03:33 +0530

    Remove final V50 compatibility explorer

--- demo_v2/server/progressiveRepositoryExplorerV50.js
+++
@@ -1,3 +0,0 @@
-// Compatibility shim for older imports. New code must import RepositoryExplorer
-// from ./repositoryExplorer.js directly.
-export { RepositoryExplorer as ProgressiveRepositoryExplorerV50 } from './repositoryExplorer.js';
