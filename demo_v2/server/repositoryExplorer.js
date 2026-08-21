ed1ae81599ace1f519272e70085a8fa8fbc789a6
thallukrish <thallukrish@gmail.com>
2026-08-21 14:34:09 +0530

    refactor: drop superseded V41 call-graph interpreter

--- demo_v2/server/repositoryExplorer.js
+++ demo_v2/server/repositoryExplorer.js
@@ -1,4 +1,4 @@
-import { ProgressiveRepositoryExplorerV41 } from './progressiveRepositoryExplorerV41.js';
+import { ProgressiveRepositoryExplorerV40 } from './progressiveRepositoryExplorerV40.js';
 import { withWholeFlowPass2 } from './explorer/wholeFlowPass2.js';
 import { withWholeFlowScheduler } from './explorer/wholeFlowScheduler.js';
 import { withScoutLifecycle } from './explorer/scoutLifecycle.js';
@@ -9,7 +9,7 @@ import { withSemanticModel } from './explorer/semanticModel.js';
 import { withBusinessPriorityScout } from './explorer/businessPriorityScout.js';
 import { withEntityReconciliation } from './explorer/entityReconciliation.js';
 
-const ExplorerWithWholeFlowPass2 = withWholeFlowPass2(ProgressiveRepositoryExplorerV41);
+const ExplorerWithWholeFlowPass2 = withWholeFlowPass2(ProgressiveRepositoryExplorerV40);
 const ExplorerWithWholeFlowScheduler = withWholeFlowScheduler(ExplorerWithWholeFlowPass2);
 const ExplorerWithScoutLifecycle = withScoutLifecycle(ExplorerWithWholeFlowScheduler);
 const ExplorerWithMapPersistence = withMapPersistence(ExplorerWithScoutLifecycle);
