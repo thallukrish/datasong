7de301fb32a71c983b982dd8c7966d5e2613a3c8
thallukrish <thallukrish@gmail.com>
2026-08-21 13:31:17 +0530

    Compose canonical explorer from V46 plus extracted V47/V48 modules

--- demo_v2/server/repositoryExplorer.js
+++ demo_v2/server/repositoryExplorer.js
@@ -1,9 +1,13 @@
-import { ProgressiveRepositoryExplorerV47 } from './progressiveRepositoryExplorerV47.js';
+import { ProgressiveRepositoryExplorerV46 } from './progressiveRepositoryExplorerV46.js';
+import { withPersistedMap } from './explorer/persistedMap.js';
+import { withStructuredWorkflow } from './explorer/structuredWorkflow.js';
 import { withSemanticModel } from './explorer/semanticModel.js';
 import { withBusinessPriorityScout } from './explorer/businessPriorityScout.js';
 import { withEntityReconciliation } from './explorer/entityReconciliation.js';
 
-const ExplorerWithSemanticModel = withSemanticModel(ProgressiveRepositoryExplorerV47);
+const ExplorerWithPersistedMap = withPersistedMap(ProgressiveRepositoryExplorerV46);
+const ExplorerWithStructuredWorkflow = withStructuredWorkflow(ExplorerWithPersistedMap);
+const ExplorerWithSemanticModel = withSemanticModel(ExplorerWithStructuredWorkflow);
 const ExplorerWithBusinessPriority = withBusinessPriorityScout(ExplorerWithSemanticModel);
 const ExplorerWithReconciliation = withEntityReconciliation(ExplorerWithBusinessPriority);
 
