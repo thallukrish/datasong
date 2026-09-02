67c347d392471e2468623f8d7d20f3f4bf02f2e7
thallukrish <thallukrish@gmail.com>
2026-09-02 07:40:30 +0530

    feat: project normalized page states

---
+++ lemap-web/src/preprocess/stateProjection.js
@@ -0,0 +1,26 @@
+export function projectPageState(snapshot = {}, preprocessed = {}) {
+  const inputs = {};
+  const options = {};
+  for (const input of preprocessed.inputs || []) {
+    let value = input.value;
+    if (snapshot.values && Object.prototype.hasOwnProperty.call(snapshot.values, input.label)) value = snapshot.values[input.label];
+    else if (snapshot.values && Object.prototype.hasOwnProperty.call(snapshot.values, input.name)) value = snapshot.values[input.name];
+    inputs[input.id] = {
+      type: input.type,
+      value: value ?? null,
+      enabled: !input.disabled,
+      visible: !!input.visible,
+      required: !!input.required,
+      readonly: !!input.readonly
+    };
+    if (input.valueDomain?.length) options[input.id] = [...input.valueDomain];
+  }
+  return {
+    pageId: preprocessed.page?.id || '',
+    route: preprocessed.page?.route || '',
+    inputs,
+    regions: { ...(snapshot.regions || {}) },
+    validations: Array.isArray(snapshot.validations) ? [...snapshot.validations] : [],
+    options
+  };
+}
