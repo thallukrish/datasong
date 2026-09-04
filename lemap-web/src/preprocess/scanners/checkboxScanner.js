6bd57956b15b2344991ffc69641a7566c4a980aa
thallukrish <thallukrish@gmail.com>
2026-09-04 23:05:34 +0530

    chore: remove obsolete scanner action metadata

--- lemap-web/src/preprocess/scanners/checkboxScanner.js
+++
@@ -1,2 +0,0 @@
-import { actionFor } from '../action.js';
-export const checkboxScanner = { actions: (input) => [actionFor(input, 'toggle', true, 'probe-toggle'), actionFor(input, 'toggle', false, 'restore-toggle')] };
