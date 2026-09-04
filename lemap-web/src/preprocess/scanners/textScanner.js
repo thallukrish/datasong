f05140e97eed8d7e9b73e1bf9699dd8460e3f077
thallukrish <thallukrish@gmail.com>
2026-09-04 23:07:38 +0530

    chore: remove obsolete scanner action metadata

--- lemap-web/src/preprocess/scanners/textScanner.js
+++
@@ -1,2 +0,0 @@
-import { actionFor } from '../action.js';
-export const textScanner = { actions: (input) => [actionFor(input, 'type', 'LeMap', 'probe-text'), actionFor(input, 'clear', '', 'probe-empty')] };
