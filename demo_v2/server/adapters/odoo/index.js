9018eedcee60aa0f64ba0b98bc1040844b328997
thallukrish <thallukrish@gmail.com>
2026-09-14 07:56:41 +0530

    feat: expose Odoo framework enricher

--- demo_v2/server/adapters/odoo/index.js
+++ demo_v2/server/adapters/odoo/index.js
@@ -1,10 +1,12 @@
 import { OdooEntitySchemaAdapter } from './schemaAdapter.js';
+import { OdooFrameworkEnricher } from './frameworkEnricher.js';
 
 export function createOdooAdapters(topology) {
   return {
     entitySchema: new OdooEntitySchemaAdapter(topology),
+    frameworkEnricher: new OdooFrameworkEnricher(topology),
     execution: null
   };
 }
 
-export { OdooEntitySchemaAdapter };
+export { OdooEntitySchemaAdapter, OdooFrameworkEnricher };
