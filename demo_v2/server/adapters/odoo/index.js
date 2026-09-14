ff286875a1dce0070f65311d36204508a3323123
thallukrish <thallukrish@gmail.com>
2026-09-14 16:06:57 +0530

    feat: expose Odoo execution adapter

--- demo_v2/server/adapters/odoo/index.js
+++ demo_v2/server/adapters/odoo/index.js
@@ -1,12 +1,13 @@
 import { OdooEntitySchemaAdapter } from './schemaAdapter.js';
 import { OdooFrameworkEnricher } from './frameworkEnricher.js';
+import { OdooExecutionAdapter } from './executionAdapter.js';
 
 export function createOdooAdapters(topology) {
   return {
     entitySchema: new OdooEntitySchemaAdapter(topology),
     frameworkEnricher: new OdooFrameworkEnricher(topology),
-    execution: null
+    execution: new OdooExecutionAdapter(topology)
   };
 }
 
-export { OdooEntitySchemaAdapter, OdooFrameworkEnricher };
+export { OdooEntitySchemaAdapter, OdooFrameworkEnricher, OdooExecutionAdapter };
