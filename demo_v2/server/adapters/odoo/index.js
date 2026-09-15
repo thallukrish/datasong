2b072a342bcf42c6fdd05f34236dc65abc753e6f
thallukrish <thallukrish@gmail.com>
2026-09-15 19:38:59 +0530

    feat: expose unified Odoo adapter facade

--- demo_v2/server/adapters/odoo/index.js
+++ demo_v2/server/adapters/odoo/index.js
@@ -1,11 +1,13 @@
 import { OdooEntitySchemaAdapter } from './schemaAdapter.js';
 import { OdooFrameworkEnricher } from './frameworkEnricher.js';
 import { OdooExecutionAdapter } from './executionAdapter.js';
+import { OdooAdapter } from './adapter.js';
 import { extractOdooUiEntrypoints } from './uiEntrypoints.js';
 import { assessOdooEvidence } from './evidenceAssessment.js';
 
 export function createOdooAdapters(topology) {
   return {
+    adapter: new OdooAdapter(topology),
     entitySchema: new OdooEntitySchemaAdapter(topology),
     frameworkEnricher: new OdooFrameworkEnricher(topology),
     execution: new OdooExecutionAdapter(topology)
@@ -13,6 +15,7 @@ export function createOdooAdapters(topology) {
 }
 
 export {
+  OdooAdapter,
   OdooEntitySchemaAdapter,
   OdooFrameworkEnricher,
   OdooExecutionAdapter,
